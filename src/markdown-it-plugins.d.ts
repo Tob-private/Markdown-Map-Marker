d275bff2dfc836538d8a7fa8f3ce2237268ece72
Tob-private <tobias.riiser.games@gmail.com>
2025-10-08 11:19:18 +0200

    removed unused md-it libraries

--- src/markdown-it-plugins.d.ts
+++ src/markdown-it-plugins.d.ts
@@ -1,5 +1,3 @@
 declare module "markdown-it-task-lists";
-declare module "markdown-it-link-attributes";
 declare module "markdown-it-mark";
 declare module "markdown-it-wikilinks";
-declare module "markdown-it-container";
