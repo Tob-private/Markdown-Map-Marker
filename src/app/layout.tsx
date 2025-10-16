d9a94d8c86a6dc3ca91b21bc3f5361cd00569fe5
Tob-private <tobias.riiser.games@gmail.com>
2025-10-16 14:49:49 +0200

    moved lucide provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import 'leaflet/dist/leaflet.css'
-
-import LucideProvider from '@/components/lucide-provider'
+import LucideProvider from '@/providers/lucide-provider'
 import Sidemenu from '@/components/navigation/sidemenu'
 
 const geistSans = Geist({
