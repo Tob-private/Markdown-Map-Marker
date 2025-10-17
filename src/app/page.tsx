5eea0ac446a176e05fffdec5db02362d39d9197d
Tob-private <tobias.riiser.games@gmail.com>
2025-10-17 10:04:18 +0200

    lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,8 @@
 import { readFile } from 'fs/promises'
 
 import MdWrapper from '@/components/md-wrapper'
-import { createServerSupabaseFromCookies } from '@/lib/db/supabase/server'
 
 export default async function Home() {
-  const supabase = await createServerSupabaseFromCookies()
-
-  const usrEmail = process.env.NEXT_PUBLIC_SUPABASE_USER_EMAIL ?? 'empty'
-  const usrPW = process.env.NEXT_PUBLIC_SUPABASE_USER_PW ?? 'empty'
-
-  const { error } = await supabase.auth.signInWithPassword({
-    email: usrEmail,
-    password: usrPW
-  })
-
-  if (error) {
-    console.error(error)
-    throw new Error('Error loging in')
-  }
-
   const rawContent = await readFile(
     'public/Markdown Map Marker/md-styling.md',
     'utf-8'
