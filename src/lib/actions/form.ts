60c345aa107d8e8305c599989f6fec6004bfb488
Tob-private <tobias.riiser.games@gmail.com>
2025-10-19 14:59:58 +0200

    broke out marker form data get to func

--- src/lib/actions/form.ts
+++ src/lib/actions/form.ts
@@ -1,6 +1,6 @@
 'use server'
 import { createServerSupabaseFromCookies } from '@/lib/db/supabase/server'
-import { CreateMapMarker } from '@/lib/types/api/leaflet'
+import { MapMarkerForm } from '@/lib/types/api/leaflet'
 import { markerFormSchema, MarkerFormState } from '@/lib/types/leaflet'
 import { revalidatePath } from 'next/cache'
 import z from 'zod'
@@ -11,24 +11,15 @@ export async function createMarker(
   formData: FormData
 ): Promise<MarkerFormState> {
   const supabase = await createServerSupabaseFromCookies()
-  const rawFormData = {
-    lat: Number(formData.get('lat')),
-    lng: Number(formData.get('lng')),
-    title: formData.get('title'),
-    desc: formData.get('desc'),
-    note_id: formData.get('note_id')
-  }
-  const validationResult = markerFormSchema.safeParse(rawFormData)
 
-  if (!validationResult.success) {
-    const errors = z.flattenError(validationResult.error).fieldErrors
-    console.dir({ errors, data: validationResult.data }, { depth: null })
-    return { success: false, errors, path: currentState.path }
+  const validationResult = getMarkerFormData(formData, currentState)
+  if (validationResult.success == false) {
+    return validationResult
   }
 
   const decimals = 5
 
-  const markerObj: CreateMapMarker = {
+  const markerObj: MapMarkerForm = {
     lat:
       Math.round(validationResult.data.lat * 10 ** decimals) / 10 ** decimals,
     lng:
@@ -37,6 +28,50 @@ export async function createMarker(
     title: validationResult.data.title,
     desc: validationResult.data.desc,
     note_id: validationResult.data.note_id
+  }
+  console.dir({ markerObj })
+
+  const { data, error } = await supabase
+    .from('map_markers')
+    .insert(markerObj)
+    .select()
+
+  if (error) {
+    console.dir({ error })
+  } else {
+    console.dir({ data })
+  }
+
+  revalidatePath(currentState.path)
+
+  return { data: validationResult.data, success: true, path: currentState.path }
+}
+export async function updateMarker(
+  img_path: string,
+  currentState: MarkerFormState,
+  formData: FormData
+): Promise<MarkerFormState> {
+  const supabase = await createServerSupabaseFromCookies()
+
+  const validationResult = getMarkerFormData(formData, currentState)
+  if (validationResult.success == false) {
+    return validationResult
+  }
+
+  const decimals = 5
+
+  const markerObj: MapMarkerForm = {
+    lat:
+      Math.round(validationResult.data.lat * 10 ** decimals) / 10 ** decimals,
+    lng:
+      Math.round(validationResult.data.lng * 10 ** decimals) / 10 ** decimals,
+    img_path,
+    title: validationResult.data.title,
+    desc: validationResult.data.desc,
+    note_id:
+      validationResult.data.note_id !== ''
+        ? validationResult.data.note_id
+        : null
   }
 
   const { data, error } = await supabase
@@ -53,4 +88,25 @@ export async function createMarker(
   revalidatePath(currentState.path)
 
   return { data: validationResult.data, success: true, path: currentState.path }
+}
+
+function getMarkerFormData(
+  formData: FormData,
+  currentState: MarkerFormState
+): MarkerFormState {
+  const rawFormData = {
+    lat: Number(formData.get('lat')),
+    lng: Number(formData.get('lng')),
+    title: formData.get('title'),
+    desc: formData.get('desc'),
+    note_id: formData.get('note_id')
+  }
+  const validationResult = markerFormSchema.safeParse(rawFormData)
+
+  if (!validationResult.success) {
+    const errors = z.flattenError(validationResult.error).fieldErrors
+    console.dir({ errors, data: validationResult.data }, { depth: null })
+    return { success: false, errors, path: currentState.path }
+  }
+  return { success: true, data: validationResult.data, path: currentState.path }
 }
--- src/lib/types/api/leaflet.ts
+++ src/lib/types/api/leaflet.ts
@@ -1,8 +1,8 @@
-export interface CreateMapMarker {
+export interface MapMarkerForm {
   lat: number
   lng: number
   img_path: string
   title: string
   desc: string
-  note_id?: string
+  note_id?: string | null
 }
