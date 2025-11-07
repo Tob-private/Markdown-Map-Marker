e65ba00bd9461465d78eeccc9b2f9553743c6b37
Tob-private <tobias.riiser.games@gmail.com>
2025-11-07 16:39:53 +0100

    fixed polygon throwing error about not being in a mapcontainer

--- src/components/leaflet/map-elements/map-polygon.tsx
+++ src/components/leaflet/map-elements/map-polygon.tsx
@@ -4,23 +4,25 @@ import Link from 'next/link'
 import styles from './map-elements.module.css'
 import { Link as LucideLink } from 'lucide-react'
 
-export const LeafletMapPolygon = dynamic(async () => {
-  const L = await import('leaflet')
-  const RL = await import('react-leaflet')
-  return function LeafletMapPolygon({ polygon }: { polygon: Polygon }) {
-    return (
-      <RL.Polygon pathOptions={polygon.options} positions={polygon.positions}>
-        <RL.Popup>
-          {polygon.note_id ? (
-            <Link href={`/${polygon.note_id}`} className={styles.popup_link}>
+export const LeafletMapPolygon = dynamic(
+  async () => {
+    const RL = await import('react-leaflet')
+    return function LeafletMapPolygon({ polygon }: { polygon: Polygon }) {
+      return (
+        <RL.Polygon pathOptions={polygon.options} positions={polygon.positions}>
+          <RL.Popup>
+            {polygon.note_id ? (
+              <Link href={`/${polygon.note_id}`} className={styles.popup_link}>
+                <h6 className={styles.popup_title}>{polygon.title}</h6>
+                <LucideLink color="var(--color-purple)" width={20} />
+              </Link>
+            ) : (
               <h6 className={styles.popup_title}>{polygon.title}</h6>
-              <LucideLink color="var(--color-purple)" width={20} />
-            </Link>
-          ) : (
-            <h6 className={styles.popup_title}>{polygon.title}</h6>
-          )}
-        </RL.Popup>
-      </RL.Polygon>
-    )
-  }
-})
+            )}
+          </RL.Popup>
+        </RL.Polygon>
+      )
+    }
+  },
+  { ssr: false }
+)
